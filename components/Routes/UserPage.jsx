3fdf5a0ef1109585c0f9d621d17f75b656e1a831
Sufyan Ghani <sufyanghani000@gmail.com>
2023-05-19 12:12:26 +0500

    Handle Delete Button to delete the user quote

--- components/Routes/UserPage.jsx
+++ components/Routes/UserPage.jsx
@@ -1,55 +1,98 @@
 import React, { useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import AddQuote from "../AddQuote";
 import UserQuotes from "../userQuotes";
 import QuoteCarousel from "../QuoteCarousel";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from "react-bootstrap/Button";
 
 function UserPage() {
+  const navigate = useNavigate()
   const location = useLocation();
   const user = location.state.user;
   const userQuotes = useRef([]);
   const [userInfo, setUserInfo] = useState({ user, userQuotes });
+  
 
   // localStorage.setItem(`${user.userName}`, JSON.stringify(userInfo));
+  const existingUser = JSON.parse(localStorage.getItem(`${user.userName}`));
+
 
   const handleQuoteSubmit = (newQuotes) => {
     console.log(newQuotes);
-    const existingUser = JSON.parse(localStorage.getItem(`${user.userName}`));
+    // const existingUser = JSON.parse(localStorage.getItem(`${user.userName}`));
     // console.log(existingUser.userQuotes.current, "current");
 
     if (existingUser) {
-      // console.log(existingUser.userQuotes.current, "inside if");
+      console.log(existingUser.userQuotes, "inside if");
       existingUser.userQuotes.current = [
         ...existingUser.userQuotes.current,
         newQuotes,
       ];
-      console.log(existingUser.userQuotes.current, "existingUser userQuotes current");
+      console.log(
+        existingUser.userQuotes,
+        "existingUser userQuotes---"
+      );
+
+
       userQuotes.current = existingUser.userQuotes.current;
       console.log(userQuotes, "User Quotes");
-      setUserInfo({ user, userQuotes });
+
+        console.log(existingUser, "existingUser")
+      setUserInfo({user, userQuotes});
+      // console.log({ existingUser, userQuotes})
       localStorage.setItem(`${user.userName}`, JSON.stringify(userInfo));
       console.log(userInfo, "userInfo");
     } else {
-      // console.log("Inside Else")
+      console.log("Inside Else")
       userQuotes.current = [...userQuotes.current, newQuotes];
       setUserInfo({ user, userQuotes });
       localStorage.setItem(`${user.userName}`, JSON.stringify(userInfo));
+      console.log(userInfo)
+
     }
+
   };
+
+
+  const handleDelete = (quoteIndex) => {
+    const updatedUserQuotes = existingUser.userQuotes.current.filter(
+      (_, index) => index !== quoteIndex
+    );
+
+    existingUser.userQuotes.current = updatedUserQuotes;
+    localStorage.setItem(`${user.userName}`, JSON.stringify(existingUser));
+    setUserInfo({ user, userQuotes: existingUser.userQuotes.current });
+  };
+
+
+
   return (
-    <CardGroup>
+    <>
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Magic Quote Generator</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+        <Button variant="outline-primary" onClick={()=> navigate('/')}>Log out</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <CardGroup className="d-flex flex-column">
       <Card>
         <QuoteCarousel />
       </Card>
-      <div className="d-flex flex-column">
-        <Card>
-          <AddQuote onQuoteSubmit={handleQuoteSubmit} />
-        </Card>
-        <UserQuotes />
-      </div>
+
+      <Card className="my-3 border-0">
+        <AddQuote onQuoteSubmit={handleQuoteSubmit} />
+      </Card>
+      {existingUser && <UserQuotes onDelete={handleDelete}/> }
     </CardGroup>
+    </>
+    
   );
 }
 
--- components/UserQuotes.jsx
+++ components/UserQuotes.jsx
@@ -1,29 +1,51 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
-
-function UserQuotes() {
+function UserQuotes({onDelete}) {
   const location = useLocation();
   const user = location.state.user;
   const currentUser = JSON.parse(localStorage.getItem(`${user.userName}`));
-  if(currentUser.userQuotes.current){
-    console.log("Inside If")
-    const userQuotes = currentUser.userQuotes.current
-  return (
-    userQuotes && <ol className="list-group list-group-numbered">
-      {userQuotes.map((userQuote, index) => (
-        <li className="list-group-item d-flex justify-content-between align-items-start" key={index}>
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">{userQuote.author}</div>
-            {userQuote.quote}
+  
+
+  // function handleDelete(quoteIndex){
+  //   console.log(quoteIndex)
+  //   console.log(currentUser)
+  //   const updatedUserQuotes = currentUser.userQuotes.current.filter(
+  //     (_, index) => index !== quoteIndex)
+    
+  //     currentUser.userQuotes.current = updatedUserQuotes;
+
+  //     localStorage.setItem(`${user.userName}`, JSON.stringify(currentUser));
+  // }
+
+
+
+  if (currentUser.userQuotes.current) {
+    console.log("Inside If");
+    const userQuotes = currentUser.userQuotes.current;
+    return (
+      userQuotes && (
+        <div className="row">
+          {userQuotes.map((userQuote, index)=>(
+            <div className="col-sm-6 my-3" key={index}>
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{userQuote.author}</h5>
+                <p className="card-text">
+                {userQuote.quote}
+                </p>
+                <button className="btn btn-primary" onClick={() => onDelete(index)}>
+                  Delete
+                </button>
+              </div>
+            </div>
           </div>
-        </li>
-      ))}
-    </ol>
-  );
+          ))}
+          
+        </div>
+      )
+    );
   }
-  
-  
 }
 
 export default UserQuotes;
